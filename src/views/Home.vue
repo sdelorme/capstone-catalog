c0423a3ae7ec8c57ca9bf79229c5c970ab355cc9
Seth Delorme <delorme.s.m@gmail.com>
2021-04-20 20:28:13 -0500

    refactor

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
-    <div v-for="student in students" v-bind:key="student.id">
-      Name: {{ student.first_name }} {{ student.last_name }}
-      <br />
-      Capstones: {{ student.capstones }}
-      <hr />
+    <div v-for="capstone in capstones" v-bind:key="capstone.id">
+      {{ capstone.student_id }}
+      <div v-for="student in students" v-bind:key="student.id">
+        <div v-if="capstone.student_id === student.id">
+          Name: {{ student.first_name }} {{ student.last_name }}
+          <br />
+          <router-link v-bind:to="`/capstones/${student.id}`">Capstones: {{ student.capstones }}</router-link>
+          <br />
+          <span><img v-bind:src="capstone.screenshot" alt="hello" /></span>
+          <hr />
+        </div>
+      </div>
     </div>
   </div>
 </template>
